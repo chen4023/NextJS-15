baba2969fd85891a435fa21662313c4b4a4af433
현채은 <ads4023@naver.com>
2024-12-24 16:36:20 +0900

    동적 라우팅/ generateStaticParams 적용

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 type Props = {
-  params: {
-    slug: string;
-  }
+  params:
+  Promise<{ slug: string; }>
 }
+// 동적 라우팅 -> 빌드시 페이지를 미리 만들어 둘 수 없음
+// 미리 만들어두고 싶으면 어떻게 해야해?
 export default async function PantsPage({ params }: Props) {
   // 15 ver에서는 비동기로 받아와야함
   const { slug } = await params
@@ -12,5 +13,12 @@ export default async function PantsPage({ params }: Props) {
       {slug} Page
     </div>
   );
+}
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map(product => ({
+    slug: product
+  }))
 }
 
