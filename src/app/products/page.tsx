bbd156fcf73ab5afbf2999f9d034b112282a9316
현채은 <ads4023@naver.com>
2025-01-07 23:29:03 +0900

    fetch를 사용한 SSG, ISR, SSR

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,10 +2,17 @@ import { getProducts } from '@/service/products';
 import Link from 'next/link';
 import React from 'react';
 
-export const revalidate = 3
+// export const revalidate = 3
 
 export default async function ProductPage() {
   const products = await getProducts()
+  const res = await fetch('https://meowfacts.herokuapp.com', {
+    cache: 'no-cache' // no-cache : SSR로 받아오겠다
+    // next:{revalidate: 0} : SSR
+    // next:{revalidate: 3} : 3초 간격 ISR
+  })
+  const data = await res.json()
+  const factText = data.data[0]
   return (
     <div>
       <h1 className='text-xl font-bold'>👚 제품 소개 페이지!</h1>
@@ -14,6 +21,7 @@ export default async function ProductPage() {
           <li key={index}><Link href={`/products/${id}`}>● {name}</Link></li>
         ))}
       </ul>
+      <article>{factText}</article>
     </div>
   );
 }
